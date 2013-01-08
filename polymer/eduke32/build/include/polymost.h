svn2github/eduke32

commit ae778d8d7a12054f26eda4d01853817c940fdc21

Fix warnings that appear if big-endian || USE_OPENGL=0.
git-svn-id: http://svn.eduke32.com/eduke32@3382 1a8010ca-5511-0410-912e-c29ae57300e0

polymer/eduke32/build/src/polymost.c
@@ -6547,10 +6547,10 @@ int32_t dxtfilter(int32_t fil, const texcachepicture *pict, const char *pic, voi
     uint32_t j, k, offs, stride, cleng;
     char *cptr;
 
-    if ((pict->format == B_LITTLE32(GL_COMPRESSED_RGB_S3TC_DXT1_EXT)) ||
-            (pict->format == B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT1_EXT))) { offs = 0; stride = 8; }
-    else if ((pict->format == B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT3_EXT)) ||
-             (pict->format == B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT5_EXT))) { offs = 8; stride = 16; }
+    if ((pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGB_S3TC_DXT1_EXT)) ||
+            (pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT1_EXT))) { offs = 0; stride = 8; }
+    else if ((pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT3_EXT)) ||
+             (pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT5_EXT))) { offs = 8; stride = 16; }
     else { offs = 0; stride = 8; }
 
     if (stride == 16) //If DXT3...
@@ -6647,10 +6647,10 @@ int32_t dedxtfilter(int32_t fil, const texcachepicture *pict, char *pic, void *m
 
     if (ispacked) inbuf = packbuf; else inbuf = midbuf;
 
-    if ((pict->format == GL_COMPRESSED_RGB_S3TC_DXT1_EXT) ||
-            (pict->format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT)) { offs = 0; stride = 8; }
-    else if ((pict->format == GL_COMPRESSED_RGBA_S3TC_DXT3_EXT) ||
-             (pict->format == GL_COMPRESSED_RGBA_S3TC_DXT5_EXT)) { offs = 8; stride = 16; }
+    if ((pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGB_S3TC_DXT1_EXT)) ||
+            (pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT1_EXT))) { offs = 0; stride = 8; }
+    else if ((pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT3_EXT)) ||
+             (pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT5_EXT))) { offs = 8; stride = 16; }
     else { offs = 0; stride = 8; }
 
     if (stride == 16) //If DXT3...
@@ -6811,10 +6811,17 @@ int32_t dedxtfilter(int32_t fil, const texcachepicture *pict, char *pic, void *m
 
 #else /* if !defined USE_OPENGL */
 
-#include "inttypes.h"
+#include "compat.h"
+
 int32_t polymost_drawtilescreen(int32_t tilex, int32_t tiley, int32_t wallnum, int32_t dimen,
                                 int32_t usehitile, uint8_t *loadedhitile)
 {
+    UNREFERENCED_PARAMETER(tilex);
+    UNREFERENCED_PARAMETER(tiley);
+    UNREFERENCED_PARAMETER(wallnum);
+    UNREFERENCED_PARAMETER(dimen);
+    UNREFERENCED_PARAMETER(usehitile);
+    UNREFERENCED_PARAMETER(loadedhitile);
     return -1;
 }
 

polymer/eduke32/build/src/defs.c
@@ -304,7 +304,9 @@ static int32_t defsparser(scriptfile *script)
             if (check_file_exist(fn))
                 break;
 
+#ifdef USE_OPENGL
             hicsetsubsttex(tile,pal,fn,-1.0,1.0,1.0,1.0,1.0,0);
+#endif
         }
         break;
         case T_DEFINESKYBOX:
@@ -323,7 +325,9 @@ static int32_t defsparser(scriptfile *script)
                     happy = 0;
             }
             if (i < 6 || !happy) break;
+#ifdef USE_OPENGL
             hicsetskybox(tile,pal,fn);
+#endif
         }
         break;
         case T_DEFINETINT:
@@ -335,7 +339,9 @@ static int32_t defsparser(scriptfile *script)
             if (scriptfile_getnumber(script,&g)) break;
             if (scriptfile_getnumber(script,&b)) break;
             if (scriptfile_getnumber(script,&f)) break; //effects
+#ifdef USE_OPENGL
             hicsetpalettetint(pal,r,g,b,f);
+#endif
         }
         break;
         case T_ALPHAHACK:
@@ -353,8 +359,11 @@ static int32_t defsparser(scriptfile *script)
         break;
         case T_ALPHAHACKRANGE:
         {
-            int32_t tilenume1,tilenume2,i;
+            int32_t tilenume1,tilenume2;
             double alpha;
+#ifdef USE_OPENGL
+            int32_t i;
+#endif
 
             if (scriptfile_getsymbol(script,&tilenume1)) break;
             if (scriptfile_getsymbol(script,&tilenume2)) break;
@@ -702,8 +711,9 @@ static int32_t defsparser(scriptfile *script)
             char *framename;
 #ifdef USE_OPENGL
             char happy=1;
+            int32_t tilex;
 #endif
-            int32_t ftilenume, ltilenume, tilex;
+            int32_t ftilenume, ltilenume;
 
             if (scriptfile_getstring(script,&framename)) break;
             if (scriptfile_getnumber(script,&ftilenume)) break; //first tile number
@@ -943,8 +953,12 @@ static int32_t defsparser(scriptfile *script)
                 case T_FRAME:
                 {
                     char *frametokptr = script->ltextptr;
-                    char *frameend, *framename = 0, happy=1;
-                    int32_t ftilenume = -1, ltilenume = -1, tilex = 0, framei;
+                    char *frameend, *framename = 0;
+#ifdef USE_OPENGL
+                    char happy=1;
+                    int32_t tilex = 0, framei;
+#endif
+                    int32_t ftilenume = -1, ltilenume = -1;
                     double smoothduration = 0.1f;
 
                     static const tokenlist modelframetokens[] =
@@ -1188,8 +1202,12 @@ static int32_t defsparser(scriptfile *script)
                 case T_HUD:
                 {
                     char *hudtokptr = script->ltextptr;
-                    char happy=1, *frameend;
-                    int32_t ftilenume = -1, ltilenume = -1, tilex = 0, flags = 0, fov = -1;
+                    char *frameend;
+#ifdef USE_OPENGL
+                    char happy=1;
+                    int32_t tilex = 0;
+#endif
+                    int32_t ftilenume = -1, ltilenume = -1, flags = 0, fov = -1;
                     double xadd = 0.0, yadd = 0.0, zadd = 0.0, angadd = 0.0;
 
                     static const tokenlist modelhudtokens[] =
@@ -1444,7 +1462,9 @@ static int32_t defsparser(scriptfile *script)
             }
             if (!happy) break;
 
+#ifdef USE_OPENGL
             hicsetskybox(tile,pal,fn);
+#endif
         }
         break;
         case T_HIGHPALOOKUP:
@@ -1583,7 +1603,9 @@ static int32_t defsparser(scriptfile *script)
                 break;
             }
 
+#ifdef USE_OPENGL
             hicsetpalettetint(pal,red,green,blue,flags);
+#endif
         }
         break;
         case T_MAKEPALOOKUP:
@@ -1704,7 +1726,9 @@ static int32_t defsparser(scriptfile *script)
                     int32_t pal=-1;
                     char *fn = NULL;
                     double alphacut = -1.0, xscale = 1.0, yscale = 1.0, specpower = 1.0, specfactor = 1.0;
+#ifdef USE_OPENGL
                     char flags = 0;
+#endif
 
                     static const tokenlist texturetokens_pal[] =
                     {
@@ -1736,10 +1760,12 @@ static int32_t defsparser(scriptfile *script)
                             scriptfile_getdouble(script,&specpower); break;
                         case T_SPECFACTOR:
                             scriptfile_getdouble(script,&specfactor); break;
+#ifdef USE_OPENGL
                         case T_NOCOMPRESS:
                             flags |= 1; break;
                         case T_NODOWNSIZE:
                             flags |= 16; break;
+#endif
                         default:
                             break;
                         }
@@ -1762,19 +1788,23 @@ static int32_t defsparser(scriptfile *script)
                     if (check_file_exist(fn))
                         break;
 
+#ifdef USE_OPENGL
                     xscale = 1.0f / xscale;
                     yscale = 1.0f / yscale;
 
                     hicsetsubsttex(tile,pal,fn,alphacut,xscale,yscale, specpower, specfactor,flags);
+#endif
                 }
                 break;
                 case T_DETAIL: case T_GLOW: case T_SPECULAR: case T_NORMAL:
                 {
                     char *detailtokptr = script->ltextptr, *detailend;
+#ifdef USE_OPENGL
                     int32_t pal = 0;
+                    char flags = 0;
+#endif
                     char *fn = NULL;
                     double xscale = 1.0, yscale = 1.0, specpower = 1.0, specfactor = 1.0;
-                    char flags = 0;
 
                     static const tokenlist texturetokens_pal[] =
                     {
@@ -1803,10 +1833,12 @@ static int32_t defsparser(scriptfile *script)
                             scriptfile_getdouble(script,&specpower); break;
                         case T_SPECFACTOR:
                             scriptfile_getdouble(script,&specfactor); break;
+#ifdef USE_OPENGL
                         case T_NOCOMPRESS:
                             flags |= 1; break;
                         case T_NODOWNSIZE:
                             flags |= 16; break;
+#endif
                         default:
                             break;
                         }
@@ -1823,6 +1855,7 @@ static int32_t defsparser(scriptfile *script)
                     if (check_file_exist(fn))
                         break;
 
+#ifdef USE_OPENGL
                     switch (token)
                     {
                     case T_DETAIL:
@@ -1841,6 +1874,7 @@ static int32_t defsparser(scriptfile *script)
                         break;
                     }
                     hicsetsubsttex(tile,pal,fn,-1.0f,xscale,yscale, specpower, specfactor,flags);
+#endif
                 }
                 break;
                 default:
@@ -1911,7 +1945,10 @@ static int32_t defsparser(scriptfile *script)
         case T_UNDEFTEXTURE:
         case T_UNDEFTEXTURERANGE:
         {
-            int32_t r0,r1,i;
+            int32_t r0,r1;
+#ifdef USE_OPENGL
+            int32_t i;
+#endif
 
             if (scriptfile_getsymbol(script,&r0)) break;
             if (tokn == T_UNDEFTEXTURERANGE)
@@ -1929,9 +1966,11 @@ static int32_t defsparser(scriptfile *script)
                     break;
             }
 
+#ifdef USE_OPENGL
             for (; r0 <= r1; r0++)
                 for (i=MAXPALOOKUPS-1; i>=0; i--)
                     hicclearsubst(r0,i);
+#endif
         }
         break;
 

polymer/eduke32/build/src/engine.c
@@ -9414,6 +9414,10 @@ static void finish_loadboard(const vec3_t *dapos, int16_t *dacursectnum, int16_t
 {
     int32_t i;
 
+#if !defined USE_OPENGL || !defined POLYMER
+    UNREFERENCED_PARAMETER(myflags);
+#endif
+
     for (i=0; i<numsprites; i++)
     {
         if ((sprite[i].cstat & 48) == 48)
@@ -13775,14 +13779,19 @@ void setbrightness(char dabrightness, uint8_t dapalid, uint8_t flags)
     int32_t i, j, nohwgamma;
     const uint8_t *dapal;
 
-    int32_t paldidchange, palsumdidchange;
+#ifdef USE_OPENGL
+    int32_t paldidchange;
+#endif
+    int32_t palsumdidchange;
 //    uint32_t lastbright = curbrightness;
 
     Bassert((flags&4)==0);
 
     if (dapalid >= basepalcount)
         dapalid = 0;
+#ifdef USE_OPENGL
     paldidchange = (curbasepal != dapalid || basepalreset);
+#endif
     curbasepal = dapalid;
     basepalreset = 0;
 

polymer/eduke32/build/src/hightile.c
@@ -294,12 +294,37 @@ int32_t hicclearsubst(int32_t picnum, int32_t palnum)
 
 #else /* USE_OPENGL */
 
-#include "inttypes.h"
+#include "compat.h"
 
-void hicsetpalettetint(int32_t palnum, char r, char g, char b, char effect) { }
-int32_t hicsetsubsttex(int32_t picnum, int32_t palnum, const char *filen, float alphacut) { return 0; }
-int32_t hicsetskybox(int32_t picnum, int32_t palnum, char *faces[6]) { return 0; }
-int32_t hicclearsubst(int32_t picnum, int32_t palnum) { return 0; }
+void hicsetpalettetint(int32_t palnum, char r, char g, char b, char effect)
+{
+    UNREFERENCED_PARAMETER(palnum);
+    UNREFERENCED_PARAMETER(r);
+    UNREFERENCED_PARAMETER(g);
+    UNREFERENCED_PARAMETER(b);
+    UNREFERENCED_PARAMETER(effect);
+}
+int32_t hicsetsubsttex(int32_t picnum, int32_t palnum, const char *filen, float alphacut)
+{
+    UNREFERENCED_PARAMETER(picnum);
+    UNREFERENCED_PARAMETER(palnum);
+    UNREFERENCED_PARAMETER(filen);
+    UNREFERENCED_PARAMETER(alphacut);
+    return 0;
+}
+int32_t hicsetskybox(int32_t picnum, int32_t palnum, char *faces[6])
+{
+    UNREFERENCED_PARAMETER(picnum);
+    UNREFERENCED_PARAMETER(palnum);
+    UNREFERENCED_PARAMETER(faces);
+    return 0;
+}
+int32_t hicclearsubst(int32_t picnum, int32_t palnum)
+{
+    UNREFERENCED_PARAMETER(picnum);
+    UNREFERENCED_PARAMETER(palnum);
+    return 0;
+}
 
 #endif
 

polymer/eduke32/build/src/sdlayer.c
@@ -205,7 +205,9 @@ void wm_setapptitle(char *name)
 int32_t main(int32_t argc, char *argv[])
 {
     int32_t r;
+#ifdef USE_OPENGL
     char *argp;
+#endif
 
     buildkeytranslationtable();
 

polymer/eduke32/build/src/winlayer.c
@@ -117,9 +117,11 @@ static inline void DI_PollJoysticks(void);
 static int32_t SetupDirectDraw(int32_t width, int32_t height);
 static void UninitDIB(void);
 static int32_t SetupDIB(int32_t width, int32_t height);
+#ifdef USE_OPENGL
 static void ReleaseOpenGL(void);
 static void UninitOpenGL(void);
 static int32_t SetupOpenGL(int32_t width, int32_t height, int32_t bitspp);
+#endif
 static BOOL RegisterWindowClass(void);
 static BOOL CreateAppWindow(int32_t modenum);
 static void DestroyAppWindow(void);
@@ -315,7 +317,9 @@ static void divcommon(int32_t *ap, int32_t *bp)
 int32_t WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int32_t nCmdShow)
 {
     int32_t r;
+#ifdef USE_OPENGL
     char *argp;
+#endif
     HDC hdc;
 
     UNREFERENCED_PARAMETER(lpCmdLine);

polymer/eduke32/source/astub.c
@@ -9206,9 +9206,10 @@ static int32_t osdcmd_tint(const osdfuncparm_t *parm)
     }
     else if (parm->numparms==0)
     {
+        palette_t empty = { 0xFF, 0xFF, 0xFF, 0x00 };
         OSD_Printf("Hightile tintings:\n");
         for (i=0,p=&hictinting[0]; i<=M32_MAXPALOOKUPS; i++,p++)
-            if (*(int32_t *)&hictinting[i] != B_LITTLE32(0x00ffffff))
+            if (*(int32_t *)&hictinting[i] != *(int32_t *)&empty)
                 OSD_Printf("pal %d: rgb %3d %3d %3d  f %d\n", i, p->r, p->g, p->b, p->f);
     }
     else if (parm->numparms>=2)

commit 2ebd43797521c46ebe406f2dc4276fd104ba0624

polymost.c: a couple of {tex,mem}cache cleanups / fixes.
- Stop using memcache on some other failure paths, particularly when
  failing to read from the on-disk texcache.
- Factor out cache reading code (3x).
- Fix endianness issues affecting big-enadian systems (one introduced by
  r3382, one existing before). Comment each B_LITTLE32 with whether we're
  converting from native to on-disk (little) endianness or back.

git-svn-id: http://svn.eduke32.com/eduke32@3386 1a8010ca-5511-0410-912e-c29ae57300e0

polymer/eduke32/build/include/polymost.h
@@ -55,8 +55,6 @@ int32_t polymost_printext256(int32_t xpos, int32_t ypos, int16_t col, int16_t ba
 
 extern float curpolygonoffset;
 
-extern int32_t cachefilehandle;
-extern FILE *cacheindexptr;
 extern int32_t cachepos;
 
 struct cacheitem_t

polymer/eduke32/build/src/polymost.c
@@ -265,12 +265,14 @@ void drawline2d(float x0, float y0, float x1, float y1, char col)
 //
 
 
-int32_t cachefilehandle = -1; // texture cache file handle
-FILE *cacheindexptr = NULL;
+static int32_t cachefilehandle = -1; // texture cache file handle
+static FILE *cacheindexptr = NULL;
 static uint8_t *memcachedata = NULL;
 static int32_t memcachesize = -1;
 int32_t cachepos = 0;
-// Set to 1 when we failed (re)allocating space for the memcache:
+// Set to 1 when we failed (re)allocating space for the memcache or failing to
+// read into it (which would presumably generate followup errors spamming the
+// log otherwise):
 static int32_t dont_alloc_memcache = 0;
 
 static hashtable_t h_texcache    = { 1024, NULL };
@@ -604,8 +606,16 @@ static void polymost_cachesync(void)
         {
             initprintf("Syncing memcache to texcache\n");
             Blseek(cachefilehandle, memcachesize, BSEEK_SET);
-            Bread(cachefilehandle, memcachedata + memcachesize, len - memcachesize);
-            memcachesize = len;
+            if (Bread(cachefilehandle, memcachedata + memcachesize, len - memcachesize) != (bssize_t)(len-memcachesize))
+            {
+                initprintf("polymost_cachesync: Failed reading texcache into memcache!\n");
+                clear_memcache();
+                dont_alloc_memcache = 1;
+            }
+            else
+            {
+                memcachesize = len;
+            }
         }
     }
 }
@@ -788,6 +798,7 @@ void polymost_glinit()
                 {
                     initprintf("Failed reading texcache into memcache!\n");
                     clear_memcache();
+                    dont_alloc_memcache = 1;
                 }
             }
         }
@@ -1411,6 +1422,29 @@ static void phex(char v, char *s)
     s[1] = x<10 ? (x+'0') : (x-10+'a');
 }
 
+// Read from on-disk texcache or its in-memory cache.
+static int32_t read_from_cache(void *dest, int32_t len)
+{
+    const int32_t ocachepos = cachepos;
+
+    cachepos += len;
+
+    if (memcachedata && memcachesize >= ocachepos+len)
+    {
+//        initprintf("using memcache!\n");
+        Bmemcpy(dest, memcachedata+ocachepos, len);
+    }
+    else
+    {
+        Blseek(cachefilehandle, ocachepos, BSEEK_SET);
+
+        if (Bread(cachefilehandle, dest, len) < len)
+            return 1;
+    }
+
+    return 0;
+}
+
 int32_t polymost_trytexcache(const char *fn, int32_t len, int32_t dameth, char effect,
                              texcacheheader *head, int32_t modelp)
 {
@@ -1447,26 +1481,16 @@ int32_t polymost_trytexcache(const char *fn, int32_t len, int32_t dameth, char e
 
 //    initprintf("Loading cached tex: %s\n", cachefn);
 
-    if (memcachedata && memcachesize >= (signed)(cachepos + sizeof(texcacheheader)))
+    if (read_from_cache(head, sizeof(texcacheheader)))
     {
-//        initprintf("using memcache!\n");
-        Bmemcpy(head, memcachedata + cachepos, sizeof(texcacheheader));
-        cachepos += sizeof(texcacheheader);
-    }
-    else
-    {
-        Blseek(cachefilehandle, cachepos, BSEEK_SET);
-        cachepos += sizeof(texcacheheader);
-        if (Bread(cachefilehandle, head, sizeof(texcacheheader)) < (int32_t)sizeof(texcacheheader))
-        {
-            err = 0;
-            goto failure;
-        }
+        err = 0;
+        goto failure;
     }
 
     // checks...
     if (Bmemcmp(head->magic, TEXCACHEMAGIC, 4)) { err=1; goto failure; }
 
+    // native (little-endian) -> internal
     head->xdim = B_LITTLE32(head->xdim);
     head->ydim = B_LITTLE32(head->ydim);
     head->flags = B_LITTLE32(head->flags);
@@ -1560,6 +1584,7 @@ void writexcache(const char *fn, int32_t len, int32_t dameth, char effect, texca
 
     if (glusetexcache == 2) head->flags |= 4;
 
+    // native -> external (little-endian)
     head->xdim = B_LITTLE32(head->xdim);
     head->ydim = B_LITTLE32(head->ydim);
     head->flags = B_LITTLE32(head->flags);
@@ -1585,6 +1610,7 @@ void writexcache(const char *fn, int32_t len, int32_t dameth, char effect, texca
 #ifdef __APPLE__
         if (pr_ati_textureformat_one && gi == 1) gi = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
 #endif
+        // native -> external (little endian)
         pict.format = B_LITTLE32(gi);
         bglGetTexLevelParameteriv(GL_TEXTURE_2D, level, GL_TEXTURE_WIDTH, &gi);
         if (bglGetError() != GL_NO_ERROR) goto failure;
@@ -1674,7 +1700,7 @@ success:
 
 static int32_t gloadtile_cached(int32_t fil, const texcacheheader *head, int32_t *doalloc, pthtyp *pth,int32_t dapalnum)
 {
-    int32_t level, r;
+    int32_t level;
     texcachepicture pict;
     char *pic = NULL, *packbuf = NULL;
     void *midbuf = NULL;
@@ -1703,20 +1729,13 @@ static int32_t gloadtile_cached(int32_t fil, const texcacheheader *head, int32_t
     // load the mipmaps
     for (level = 0; level==0 || (pict.xdim > 1 || pict.ydim > 1); level++)
     {
-        if (memcachedata && memcachesize >= (signed)(cachepos + sizeof(texcachepicture)))
+        if (read_from_cache(&pict, sizeof(texcachepicture)))
         {
-            //        initprintf("using memcache!\n");
-            Bmemcpy(&pict, memcachedata + cachepos, sizeof(texcachepicture));
-            cachepos += sizeof(texcachepicture);
-        }
-        else
-        {
-            Blseek(fil, cachepos, BSEEK_SET);
-            r = Bread(fil, &pict, sizeof(texcachepicture));
-            cachepos += sizeof(texcachepicture);
-            if (r < (int32_t)sizeof(texcachepicture)) { err=1; goto failure; }
+            err=1;
+            goto failure;
         }
 
+        // external (little endian) -> native
         pict.size = B_LITTLE32(pict.size);
         pict.format = B_LITTLE32(pict.format);
         pict.xdim = B_LITTLE32(pict.xdim);
@@ -6500,6 +6519,7 @@ static void dxt_handle_io(int32_t fil, int32_t len, void *midbuf, char *packbuf)
         writebuf = midbuf;
     }
 
+    // native -> external (little endian)
     j = B_LITTLE32(cleng);
     Bwrite(fil, &j, sizeof(j));
     Bwrite(fil, writebuf, cleng);
@@ -6511,24 +6531,15 @@ static int32_t dedxt_handle_io(int32_t fil, int32_t j /* TODO: better name */,
     void *inbuf;
     int32_t cleng;
 
-    if (memcachedata && memcachesize >= (signed)(cachepos + sizeof(int32_t)))
-    {
-        cleng = *(int32_t *)(memcachedata + cachepos);
-        cachepos += sizeof(int32_t);
-    }
-    else
-    {
-        Blseek(fil, cachepos, BSEEK_SET);
-        cachepos += sizeof(int32_t);
-        if (Bread(fil, &cleng, sizeof(int32_t)) < (signed)sizeof(int32_t))
-            return -1;
+    if (read_from_cache(&cleng, sizeof(int32_t)))
+        return -1;
 
-        cleng = B_LITTLE32(cleng);
-    }
+    // external (little endian) -> native
+    cleng = B_LITTLE32(cleng);
 
     inbuf = (ispacked && cleng < j) ? packbuf : midbuf;
 
-    if (memcachedata && memcachesize >= (signed)(cachepos + cleng))
+    if (memcachedata && memcachesize >= cachepos + cleng)
     {
         if (ispacked && cleng < j)
         {
@@ -6596,6 +6607,7 @@ Description of Ken's filter to improve LZW compression of DXT1 format by ~15%: (
  I think this improved compression by a few % :)
  */
 
+// NOTE: <pict> members are in external (little) endianness.
 int32_t dxtfilter(int32_t fil, const texcachepicture *pict, const char *pic, void *midbuf, char *packbuf, uint32_t miplen)
 {
     uint32_t j, k, offs, stride;
@@ -6643,16 +6655,16 @@ int32_t dxtfilter(int32_t fil, const texcachepicture *pict, const char *pic, voi
     return 0;
 }
 
+// NOTE: <pict> members are in native endianness.
 int32_t dedxtfilter(int32_t fil, const texcachepicture *pict, char *pic, void *midbuf, char *packbuf, int32_t ispacked)
 {
     int32_t j, k, offs, stride;
     char *cptr;
 
-    if ((pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGB_S3TC_DXT1_EXT)) ||
-            (pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT1_EXT))) { offs = 0; stride = 8; }
-    else if ((pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT3_EXT)) ||
-             (pict->format == (signed) B_LITTLE32(GL_COMPRESSED_RGBA_S3TC_DXT5_EXT))) { offs = 8; stride = 16; }
-
+    if ((pict->format == GL_COMPRESSED_RGB_S3TC_DXT1_EXT) ||
+            (pict->format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT)) { offs = 0; stride = 8; }
+    else if ((pict->format == GL_COMPRESSED_RGBA_S3TC_DXT3_EXT) ||
+             (pict->format == GL_COMPRESSED_RGBA_S3TC_DXT5_EXT)) { offs = 8; stride = 16; }
     else { offs = 0; stride = 8; }
 
     if (stride == 16) //If DXT3...
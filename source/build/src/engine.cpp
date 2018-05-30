svn2github/eduke32

commit 3f597841033cb7631dbfff93a3665ae9f9d6e4ca

engine.cpp: fix issue with voxmodel_t voxels not having their scale set properly due to defs being processed before PolymostProcessVoxels()
git-svn-id: http://svn.eduke32.com/eduke32@6905 1a8010ca-5511-0410-912e-c29ae57300e0

source/build/src/engine.cpp
@@ -9842,6 +9842,7 @@ static void PolymostProcessVoxels(void)
         if (voxfilenames[i])
         {
             voxmodels[i] = voxload(voxfilenames[i]);
+            voxmodels[i]->scale = voxscale[i]>>16;
             DO_FREE_AND_NULL(voxfilenames[i]);
         }
     }

commit 1a0569c6b78a03cd2f183cd0b71222316edcbbb8

engine.cpp: fix issue introduced in r6905 where voxels with fractional scaling would not be handled correctly in Polymost
git-svn-id: http://svn.eduke32.com/eduke32@6907 1a8010ca-5511-0410-912e-c29ae57300e0

source/build/src/engine.cpp
@@ -9842,7 +9842,7 @@ static void PolymostProcessVoxels(void)
         if (voxfilenames[i])
         {
             voxmodels[i] = voxload(voxfilenames[i]);
-            voxmodels[i]->scale = voxscale[i]>>16;
+            voxmodels[i]->scale = voxscale[i]*(1.f/65536.f);
             DO_FREE_AND_NULL(voxfilenames[i]);
         }
     }